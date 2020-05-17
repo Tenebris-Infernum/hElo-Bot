d4dadc19fe2a65bf9fdef625ff60bcd808d00243
Tenebris-Infernum <64001826+Tenebris-Infernum@users.noreply.github.com>
2020-05-17 16:25:28 +0400

    Update fishLord.py

--- fishLord.py
+++ fishLord.py
@@ -5,32 +5,40 @@ import time
 
 client = commands.Bot(command_prefix = ('h', 'H'))
 shameRole = 'Pit Of Shame'
+timeAmount = time.time() + 60
 
+#When the bot has connected to the server
 @client.event
 async def on_ready():
     print('Bot is ready.')
 
+#When a member has joins the server
 @client.event
 async def on_member_join(member):
     await member.send(f'Welcome {member} to the fish cult.')
 
+#When a member leaves the server
 @client.event
 async def on_member_remove(member):
     await member.send(f'You have been kicked from the fish cult for you have displeased the fish lord .')
 
+#When a certain word in a message is detected
 @client.event
 async def on_message(message):
-
+    
     #Fish Helo Meme
     if 'helo' in message.content.lower():
-        await message.channel.send(str(message.author) + ' has summoned the fish lord.\nhttps://imgur.com/a/BxMCgvh')
+        username = str(message.author).split('#', 1)
+        await message.channel.send(str(username[0]) + ' has summoned the fish lord. \nhttps://imgur.com/a/BxMCgvh')
 
     #UWU Moderation
     if 'uwu' in message.content.lower():
-        await message.channel.send(str(message.author) + ' has uttered the forbidden word. You shall now persish.')
+        username = str(message.author).split('#', 1)
+        await message.channel.send(str(username[0]) + ' has uttered the forbidden word. Perish, mortal.')
         role = get(message.author.guild.roles, name=shameRole)
-        await message.author.add_roles(role)
-        time.sleep(60)
+        while time.time() < timeAmount:
+            await message.author.add_roles(role)
         await message.author.remove_roles(role)
 
-client.run('enter token here')
+#Token used to connect code to bot     
+client.run('<insert token here>')
